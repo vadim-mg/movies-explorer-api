114bca4ea6bdbcd3d3371474f6b1e1bd0ce94d6b
Vadim <vadikm1981@gmail.com>
2021-08-17 12:07:19 +0500

    feat: config.js

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+const DEVELOPMENT_ALLOWED_CORS = "['http://localhost:3001', 'https://localhost:3001']";
+const DEVELOPMENT_SECRET = 'dev-secret';
+
+module.exports = {
+  secretKey: process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : DEVELOPMENT_SECRET,
+  allowedCors: process.env.NODE_ENV === 'production' ? process.env.ALLOWED_CORS : DEVELOPMENT_ALLOWED_CORS,
+  port: process.env.NODE_ENV === 'production' ? process.env.PORT : 3000,
+  dataBase: 'mongodb://localhost:27017/moviesdb',
+};
