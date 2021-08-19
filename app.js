d4779dcff1cb95eb2b6410e71812a44b46d38bbf
Vadim <vadikm1981@gmail.com>
2021-08-19 12:06:08 +0500

    helmet and cors@

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const { json, urlencoded } = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -10,6 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { routes } = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { dataBase, port } = require('./utils/config');
+const { cors } = require('./middlewares/cors');
 
 mongoose.connect(dataBase, {
   useNewUrlParser: true,
@@ -19,6 +21,10 @@ mongoose.connect(dataBase, {
 });
 
 const app = express();
+
+app.use(cors);
+app.use(helmet());
+app.disable('x-powered-by');
 
 app.use(
   requestLogger,
@@ -30,7 +36,7 @@ app.use(
 app.use(routes);
 
 app.use(
-  errorLogger,
+  errorLogger, // логирование ошибок
   errors(), // ошибки валидации celebrate
   errorHandler, // общий обработчик ошибок
 );
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,14 +3,14 @@ const { Error400, Error500 } = require('../errors/errors');
 module.exports = (err, req, res, next) => {
   let error = err;
 
-  // обрабатываем 400-е ошибки, вызванные не Celebrete
-  if (err.statusCode === 400 || err.name === 'CastError') {
-    error = new Error400(`Ошибка запроса: ${err.message}`);
+  // обрабатываем ошибки относящиеся к статусу 400, но которые не были пойманы Celebrete
+  if (err.statusCode === 400 || err.name === 'CastError' || err.name === 'ValidationError') {
+    error = new Error400(`Ошибка в запросе: ${err.message}`);
   }
 
-  // обрабатываем ошибку сервера
-  if (err.statusCode === 500) {
-    error = new Error500(`На сервере произошла ошибка: ${err.message}`);
+  // все остальные ошибки, возвращаем как ошибку сервера
+  if (!error.statusCode) {
+    error = new Error500('На сервере произошла какя-то ошибка!');
   }
 
   res.status(error.statusCode).send({ message: error.message });
