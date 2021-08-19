f23a328b890b6ac397cdadf7bc3989a4f8504a2a
Vadim <vadikm1981@gmail.com>
2021-08-19 11:59:46 +0500

    feat: .env-axample and config fix

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,21 @@
 const DEVELOPMENT_ALLOWED_CORS = "['http://localhost:3001', 'https://localhost:3001']";
 const DEVELOPMENT_SECRET = 'dev-secret';
+const DEVELOPMENT_DBASE = 'mongodb://localhost:27017/moviesdb';
+const DEVELOPMENT_PORT = 3000;
+
+const prodMode = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  secretKey: process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : DEVELOPMENT_SECRET,
-  allowedCors: process.env.NODE_ENV === 'production' ? process.env.ALLOWED_CORS : DEVELOPMENT_ALLOWED_CORS,
-  port: process.env.NODE_ENV === 'production' ? process.env.PORT : 3000,
-  dataBase: 'mongodb://localhost:27017/moviesdb',
+  secretKey: prodMode
+    ? process.env.JWT_SECRET
+    : DEVELOPMENT_SECRET,
+  allowedCors: prodMode
+    ? process.env.ALLOWED_CORS
+    : DEVELOPMENT_ALLOWED_CORS,
+  port: prodMode
+    ? process.env.PORT
+    : DEVELOPMENT_PORT,
+  dataBase: prodMode
+    ? process.env.DATA_BASE
+    : DEVELOPMENT_DBASE,
 };
